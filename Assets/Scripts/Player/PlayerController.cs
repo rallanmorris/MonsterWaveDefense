43375f360e4db9d98468c698ef1cb44bde4763ec
Richard Morris <rallanmorris@gmail.com>
2021-09-29 16:37:15 -0400

    First Commit Basic Castle Environment with functioning 3rd Person Player controller by Gamepad

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+    [SerializeField] float speed = 6f;
+    [SerializeField] float turnSmoothTime = 0.1f;
+    [SerializeField] Transform playerCamera;
+    [SerializeField] float gravity = -9.8f;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundDistance = 0.4f;
+    [SerializeField] LayerMask groundMask;
+    [SerializeField] float jumpHeight = 3f;
+
+    private float turnSmoothVelocity;
+    private Vector2 moveInput;
+    Vector3 velocity;
+    bool isGrounded;
+    bool jumpButtonDown = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float horizontal = moveInput.x;
+        float vertical = moveInput.y;
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + playerCamera.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+        
+        if(jumpButtonDown && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    public void Movement(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.ReadValue<float>() > .1f)
+            jumpButtonDown = true;
+        else
+            jumpButtonDown = false;
+    }
+}
